d49f40cb0ff72d3578059026d5274048256bd83c
Dylan Choque Nina <113360472+Dylann55@users.noreply.github.com>
2023-09-05 22:01:09 -0300

    Update supaBase.js

--- database/supaBase.js
+++ database/supaBase.js
@@ -1,6 +1,5 @@
 /* eslint-disable no-console */
 /* eslint-disable no-useless-constructor */
-import { createClient } from '@supabase/supabase-js';
 import env from 'dotenv';
 
 env.config();
@@ -29,38 +28,7 @@ class SupaBase extends DataBase {
   }
 
   async connect() {
-    try {
-      const dataBase = createClient(this.dataBaseUrl, this.dataKey, {
-        auth: {
-          autoRefreshToken: false,
-          persistSession: false,
-        },
-      });
-      const { data } = await dataBase.from('status').select();
-      if (data) {
-        console.log('Conectado a la Primera Base de Datos');
-        return dataBase;
-      }
-    } catch (err) {
-      console.error(`Error al conectar a la base de datos 1: ${err}`);
-    }
-
-    try {
-      const dataBaseBackup = createClient(this.dataBaseUrlBackup, this.dataKeyBackup, {
-        auth: {
-          autoRefreshToken: false,
-          persistSession: false,
-        },
-      });
-      const { data } = await dataBaseBackup.from('status').select();
-
-      if (data) {
-        console.log('Conectado a la Segunda Base de Datos');
-        return dataBaseBackup;
-      }
-    } catch (err) {
-      console.error(`Error al conectar a la base de datos 2: ${err}`);
-    }
+    console.log("xd")
 
     return null;
   }
