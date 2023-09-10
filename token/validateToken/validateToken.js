b2aad6f5e7e4d7f4c4ac7a0c9d6a78dd8a78e37b
Dylan Choque Nina <113360472+Dylann55@users.noreply.github.com>
2023-09-10 18:37:57 -0300

    Frontd

--- token/validateToken/validateToken.js
+++ token/validateToken/validateToken.js
@@ -2,6 +2,7 @@
 import decodeToken from '../decodeToken/decodeToken.js';
 
 const validateToken = (req, res, next) => {
+  console.log(req);
   const authHeader = req.headers.authorization;
   const token = authHeader && authHeader.split(' ')[1];
   if (!token) { return res.status(401).json({ message: 'No se proporciono el token' }); }
