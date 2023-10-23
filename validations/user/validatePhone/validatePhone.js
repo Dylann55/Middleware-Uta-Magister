89ba75633731b0dd81ab6b559626ed80ef733acc
Dylan Choque Nina <113360472+Dylann55@users.noreply.github.com>
2023-10-22 21:26:17 -0300

    phone

--- validations/user/validatePhone/validatePhone.js
+++ validations/user/validatePhone/validatePhone.js
@@ -2,8 +2,8 @@ import { body, validationResult } from 'express-validator';
 
 const validatePhone = [
   body('phone')
-    .matches(/^\d{10}$/) // Verifica si el número de teléfono tiene 10 dígitos
-    .withMessage('El número de teléfono debe contener exactamente 10 dígitos'),
+    .matches(/^\d{9}$/)
+    .withMessage('El número de teléfono debe contener exactamente 9 dígitos(incluir el 9 inicial)'),
   (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
