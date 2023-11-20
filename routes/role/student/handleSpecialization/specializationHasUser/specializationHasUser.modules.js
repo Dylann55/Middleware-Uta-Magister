70bf7cc05c7d0983268379e0ea094998ef4fbbc0
Dylan Choque Nina <113360472+Dylann55@users.noreply.github.com>
2023-11-20 11:04:19 -0300

    Update specializationHasUser.modules.js

--- routes/role/student/handleSpecialization/specializationHasUser/specializationHasUser.modules.js
+++ routes/role/student/handleSpecialization/specializationHasUser/specializationHasUser.modules.js
@@ -23,6 +23,6 @@ export {
   verifyStatus,
   validateArraySpecializationHasUserID,
   validateSpecializationHasUserID,
-  validateSemesterID,
   validateSpecializationID,
+  validateSemesterID,
 };
