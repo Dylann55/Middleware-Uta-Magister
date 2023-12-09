3713632468bdd14117aaafcb6a2967bda55bbf7d
Dylan Choque Nina <113360472+Dylann55@users.noreply.github.com>
2023-12-09 16:23:54 -0300

    update academic handleRubric

--- routes/role/academic/academic.js
+++ routes/role/academic/academic.js
@@ -4,10 +4,12 @@ import {
   firstStageRoute,
   secondStageRoute,
   listSemester,
+  handleRubric,
 } from './academic.modules.js';
 
 const academicRoute = express.Router();
 academicRoute.use('/firstStage', firstStageRoute);
 academicRoute.use('/secondStage', secondStageRoute);
 academicRoute.get('/semester', listSemester);
+academicRoute.get('/handleRubric', handleRubric);
 export default academicRoute;
--- routes/role/academic/academic.modules.js
+++ routes/role/academic/academic.modules.js
@@ -3,10 +3,12 @@ import express from 'express';
 import firstStageRoute from './firstStage/firstStage.js';
 import secondStageRoute from './secondStage/secondStage.js';
 import listSemester from '../handleShared/semester/listSemester/listSemester.js';
+import handleRubric from '../handleShared/handleRubric/handleRubricRoute.js';
 
 export {
   express,
   firstStageRoute,
   secondStageRoute,
   listSemester,
+  handleRubric,
 };
